b86f1d5f9fad841d8ad32e4949eb56edd774cc13
giabao0301 <21521866@gm.uit.edu.vn>
2024-12-19 14:51:50 +0700

    Swap rent and sale listings in Home component for correct display

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -54,7 +54,7 @@ export default function Home() {
       {/* listing results for offer, sale and rent */}
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8">
-        {rentListings && rentListings.length > 0 && (
+        {saleListings && saleListings.length > 0 && (
           <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
@@ -68,13 +68,13 @@ export default function Home() {
               </Link>
             </div>
             <div className="flex flex-wrap gap-4">
-              {rentListings.map((listing) => (
+              {saleListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
             </div>
           </div>
         )}
-        {saleListings && saleListings.length > 0 && (
+        {rentListings && rentListings.length > 0 && (
           <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
@@ -88,7 +88,7 @@ export default function Home() {
               </Link>
             </div>
             <div className="flex flex-wrap gap-4">
-              {saleListings.map((listing) => (
+              {rentListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
             </div>
